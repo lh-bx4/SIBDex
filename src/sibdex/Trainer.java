09fda76b24fd6710e5d69040b5403f842b36c89e
Windows <Windows@DESKTOP-KND2T5K.home>
2020-06-01 18:40:39 +0200

    updating Trainer class

--- src/sibdex/Trainer.java
+++ src/sibdex/Trainer.java
@@ -11,12 +11,13 @@ package sibdex;
  */
 public class Trainer {
     private int id, age ;
-    private String name;
+    private String name, region ;
 
-    public Trainer(int id, int age, String name) {
+    public Trainer(int id, int age, String name, String region) {
         this.id = id;
         this.age = age;
         this.name = name;
+        this.region = region;
     }
 
     public int getId() {
@@ -41,6 +42,14 @@ public class Trainer {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
     }
     
 }
